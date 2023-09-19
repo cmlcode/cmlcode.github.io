4121dfe7d09e5568eef45aacf079b5a8ac8da507
Chris Lewis <61091285+cmlcode@users.noreply.github.com>
2023-09-19 15:45:00 +1000

    Added mobile hamburger menu

---
+++ index.js
@@ -0,0 +1,20 @@
+const menu_wrapper = document.querySelector(".header_menu_wrapper")
+const menu_button = document.querySelector(".header_menu")
+const open_menu_button = document.querySelector(".header_menu_open")
+const menu_box = document.querySelector(".header_expanded")
+
+menu_wrapper.addEventListener('click', () => {
+    //Open link bar
+    
+    //Change menu button
+    if (menu_button.classList.contains("invisible")){
+        menu_button.classList.remove("invisible");
+        open_menu_button.classList.add("invisible");
+        menu_box.classList.add("invisible")
+    }
+    else{
+        menu_button.classList.add("invisible");
+        open_menu_button.classList.remove("invisible");
+        menu_box.classList.remove("invisible")
+    }
+  })
