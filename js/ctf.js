ba69d20f8f4b931d68a850a945a5483abceb9916
Chris Lewis <61091285+cmlcode@users.noreply.github.com>
2024-02-25 09:02:14 -0600

    Add web exploits

--- js/ctf.js
+++ js/ctf.js
@@ -1,6 +1,7 @@
 const answers = new Map();
 answers.set("answer1", "i love cookies");
 answers.set("answer2", "congrats");
+answers.set('answer4', 'you found me!');
 
 function showHint(hint_element_id){
     const hint_element = document.getElementById(hint_element_id);
@@ -14,8 +15,8 @@ function showHint(hint_element_id){
 function checkAnswer(answer_element_id){
     const userInput = document.getElementById(answer_element_id).value.toLowerCase();
     if (userInput == answers.get(answer_element_id)){
-        alert("correct")
+        alert("correct");
         return;
     }
-    alert("wrong")
+    alert("wrong");
 }
